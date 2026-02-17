6cf486dc9c1a921062ccda6f853e14e71392fe55
rohitkarale60-create <rohitkarale60@gmail.com>
2026-02-17 23:43:37 +0530

    Add files via upload
    Add Task 1 and Task 2 Python files

---
+++ task 2.py
@@ -0,0 +1,9 @@
+# addition of 1 to 50
+
+total= 0
+
+# Using for loop
+for number in range(1, 51):
+    total += number
+
+print("The sum of numbers from 1 to 50 is:", total)
---
+++ task1.py
@@ -0,0 +1,8 @@
+# we can use here if and else to check
+number = int(input("Enter an integer: "))
+
+# Checking even or odd
+if number % 2 == 0:
+    print(number, "is an Even number.")
+else:
+    print(number, "is an Odd number.")
